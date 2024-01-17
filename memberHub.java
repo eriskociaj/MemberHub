47c74e985d641aa8973a40b488e6007f45e2039d
Eris Kociaj <eris.kociaj@elev.ga.ntig.se>
2024-01-17 11:51:54 +0100

    first commit

---
+++ memberHub.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class Person {
+    private int id;
+    private String name;
+    private String contactInformation;
+
+    public Person(int id, String name, String contactInformation) {
+        this.id = id;
+        this.name = name;
+        this.contactInformation = contactInformation;
+    }
+}
