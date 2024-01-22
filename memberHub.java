e1241a9f91b084aec991688bf8ee2433f07014aa
Eris Kociaj <eris.kociaj@elev.ga.ntig.se>
2024-01-22 18:33:05 +0100

    commit 2!

--- memberHub.java
+++ memberHub.java
@@ -10,4 +10,52 @@ class Person {
         this.name = name;
         this.contactInformation = contactInformation;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContactInformation() {
+        return contactInformation;
+    }
+
+    public void updateDetails(String newName, String newContactInformation) {
+        this.name = newName;
+        this.contactInformation = newContactInformation;
+        System.out.println("Details updated for person with ID " + id);
+    }
+
+    public void deletePerson() {
+        System.out.println("Person with ID " + id + " deleted.");
+    }
+}
+
+class WorkTime {
+    private int id;
+    private int personId;
+    private String date;
+    private String inTime;
+    private String outTime;
+    private double overtime;
+
+    public WorkTime(int id, int personId, String date, String inTime, String outTime, double overtime) {
+        this.id = id;
+        this.personId = personId;
+        this.date = date;
+        this.inTime = inTime;
+        this.outTime = outTime;
+        this.overtime = overtime;
+    }
+
+    public void logWorkTime() {
+        System.out.println("Work time logged for person with ID " + personId + " on " + date);
+    }
+
+    public void generateReport() {
+        System.out.println("Report generated for work time on " + date);
+    }
 }
